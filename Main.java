d1936981cd74b2d6621e9a639071212c71b07ab3
Nick <nicholas.strazicic2027@gbwl.org>
2026-09-18 16:35:17 +0000

    added some notes on JAVA vs JAVASCRIPT and added some vocab

--- Main.java
+++ Main.java
@@ -6,10 +6,18 @@ public class Main {
 }
 
 /* COMMENTS FOR NOTE TAKING!!! 
-   aye dio mio
+   Algorithms: Step by step proccess to accomp a task
+   Psuedocode: Simplified code to outline programs/algorithms (fake code)
+   Sequencing: The order of steps
 
 
+   JAVASCRIPT vs JAVA
+   JAVA:
+   - You can use Java to create complex applications like mobile apps and enterprise software
+   -
 
-
+   JAVASCRIPT:
+   - JavaScript helps front-end developers build interactive web pages
+   -
 
 */
