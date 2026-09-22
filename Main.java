6543800c83f2b195c3315aa3af97877eb5bd4ec3
Nick <nicholas.strazicic2027@gbwl.org>
2026-09-22 13:41:02 +0000

    added notes and experimented with errors

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      System.out.println("It makes no sense to divide a number by zero!");
+      System.out.println(3/0);
+
+
+
    }
 }
 
 /* COMMENTS FOR NOTE TAKING!!! 
+   9/18/26
    Algorithms: Step by step proccess to accomp a task
    Psuedocode: Simplified code to outline programs/algorithms (fake code)
    Sequencing: The order of steps
@@ -14,10 +19,23 @@ public class Main {
    JAVASCRIPT vs JAVA
    JAVA:
    - You can use Java to create complex applications like mobile apps and enterprise software
-   -
+   
 
    JAVASCRIPT:
    - JavaScript helps front-end developers build interactive web pages
-   -
+   
+   9/22/26
+
+   Object-oriented programming: programming built on classes and objects
+   Classes: blueprint of an object (no memory)
+   Object: actual implementation (stored in memory)
+   Method: reusable chunk of code that accomplishes an action (verbs in code)
+   |-- Main() entry point to our code
+   We code in an IDE (intergrated Development Enviornment) with a compiler translate our java to binary 
+   Every action in java ends with a semi-colon 
+
 
 */
+
+// --> line comment
+/* bulk comment */
