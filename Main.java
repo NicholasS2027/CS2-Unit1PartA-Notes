9c7bce24d44ef29597040d2c206840602d06d735
Nick <nicholas.strazicic2027@gbwl.org>
2026-09-23 18:36:00 +0000

    added notes

--- Main.java
+++ Main.java
@@ -33,7 +33,30 @@ public class Main {
    |-- Main() entry point to our code
    We code in an IDE (intergrated Development Enviornment) with a compiler translate our java to binary 
    Every action in java ends with a semi-colon 
+      
 
+   9/23/26
+
+   Primitive type - storing simple information/data
+   Object (Reference) type - storing complex data/objects (ex. Creature cat = new Creature())
+
+   Primitive Variable Types to Know:
+   1. int - stores integers/positive or negative whole numbers
+   2. double - store decimal numbers (ex. double x = 5.0;) (ex. double y = 4.25;)
+   3. boolean - stores logic (only 2 options are true or false)
+
+   Object Variable Types to Know:
+   1. String - stores text between quotes (ex. "5.0", "Hello world!")
+
+   Setting Up Variables in Code:
+   Declaring and Assigning go together
+   1. Declare Variable ---> int x; String name;
+   2. Assign Variable ---> x = 5; name = "Nick"
+   
+
+   
+   Or do it in one step
+   3. Initialize Variable ---> int x = 5; String name = "Nick"
 
 */
 
